cbe75f4ffb9e74da77581a799afdee3313615e70
Yunho <potatohead12@naver.com>
2021-08-17 00:13:16 +0900

    23

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -2,11 +2,11 @@ import 'package:flutter_blog/domain/user/user_repository.dart';
 import 'package:flutter_blog/util/jwt.dart';
 import 'package:get/get.dart';
 
-final UserRepositroy _userRepositroy = UserRepositroy();
+final UserRepository _userRepository = UserRepository();
 
 class UserController extends GetxController {
   Future<void> login(String username, String password) async {
-    String token = await _userRepositroy.login(username, password);
+    String token = await _userRepository.login(username, password);
     jwtToken = token;
     print("jwtToken: $jwtToken");
   }
--- lib/domain/user/user_repository.dart
+++ lib/domain/user/user_repository.dart
@@ -2,12 +2,13 @@ import 'package:flutter_blog/controller/dto/loginRequestDto.dart';
 import 'package:flutter_blog/domain/user/user_provider.dart';
 import 'package:get/get.dart';
 
-class UserRepositroy {
+class UserRepository {
   final UserProvider _userProvider = UserProvider();
   Future<String> login(String username, String password) async {
     LoginReqDto loginReqDto = LoginReqDto(username, password);
     print(loginReqDto.toJson());
     Response response = await _userProvider.login(loginReqDto.toJson());
+    print("response.body ${response.body}");
     dynamic headers = response.headers;
     String token = headers["authorization"];
     return token;
